e7e63a5254d177617e786296eea0c87d410c9c75
chirannnn <chiranyt117@gmail.com>
2025-09-30 22:49:45 +0530

    update leetcode

---
+++ problems/05-searching-easy/src/com/leetcode/IntersectionOfTwoArrays12.java
@@ -0,0 +1,31 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ */
+
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class IntersectionOfTwoArrays12 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+
+        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+    }
+
+    static int[] intersection(int[] nums1, int[] nums2) {
+        // brute-force
+        // mudila da gothaaaa
+        return new int[]{};
+    }
+}
